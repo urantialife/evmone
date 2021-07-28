cac511b9071001d18897900235fe6cb6425c63da
Andrei Maiboroda <andrei@ethereum.org>
2021-07-28 16:03:22 +0200

    Tests for EOF code validation

--- test/unittests/eof_validation_test.cpp
+++ test/unittests/eof_validation_test.cpp
@@ -88,3 +88,58 @@ TEST(eof_validation, EOF1_multiple_data_sections)
     EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010001 020001 020001 00 FE DA DA")),
         EOFValidationErrror::multiple_data_sections);
 }
+
+TEST(eof_validation, EOF1_undefined_opcodes)
+{
+    // TODO replace with loop over evmone::instr::gas_costs
+
+    EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010001 00 0C")),
+        EOFValidationErrror::undefined_instruction);
+    EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010001 00 21")),
+        EOFValidationErrror::undefined_instruction);
+    EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010001 00 B0")),
+        EOFValidationErrror::undefined_instruction);
+    EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010001 00 C0")),
+        EOFValidationErrror::undefined_instruction);
+    EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010001 00 EF")),
+        EOFValidationErrror::undefined_instruction);
+
+    EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010001 00 FE")), EOFValidationErrror::success);
+}
+
+TEST(eof_validation, EOF1_truncated_push)
+{
+    // TODO replace with loops
+
+    EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010001 00 60")), EOFValidationErrror::truncated_push);
+
+    EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010001 00 61")), EOFValidationErrror::truncated_push);
+    EXPECT_EQ(
+        validate_eof(from_hex("EFCAFE01 010002 00 6100")), EOFValidationErrror::truncated_push);
+
+    EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010001 00 62")), EOFValidationErrror::truncated_push);
+    EXPECT_EQ(
+        validate_eof(from_hex("EFCAFE01 010002 00 6200")), EOFValidationErrror::truncated_push);
+    EXPECT_EQ(
+        validate_eof(from_hex("EFCAFE01 010003 00 620000")), EOFValidationErrror::truncated_push);
+
+    EXPECT_EQ(
+        validate_eof(from_hex(
+            "EFCAFE01 010020 00 7F00000000000000000000000000000000000000000000000000000000000000")),
+        EOFValidationErrror::truncated_push);
+}
+
+TEST(eof_validation, EOF1_complete_push)
+{
+    // TODO replace with loop
+
+    EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010002 00 6000")), EOFValidationErrror::success);
+    EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010003 00 610000")), EOFValidationErrror::success);
+    EXPECT_EQ(validate_eof(from_hex("EFCAFE01 010004 00 62000000")), EOFValidationErrror::success);
+    EXPECT_EQ(
+        validate_eof(from_hex("EFCAFE01 010005 00 6300000000")), EOFValidationErrror::success);
+    EXPECT_EQ(validate_eof(
+                  from_hex("EFCAFE01 010021 00 "
+                           "7F0000000000000000000000000000000000000000000000000000000000000000")),
+        EOFValidationErrror::success);
+}
